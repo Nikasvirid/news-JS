dcc1b0bbba2c16c5edebdeff4dabf19a9e01d75a
Nikasvirid <sviridka-vp@mail.ru>
2022-06-29 23:11:46 +0300

    feat:add APIkey

--- src/components/controller/appLoader.js
+++ src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: '9c981a17c97342a2b7b6ab85e4c870e3', // получите свой ключ https://newsapi.org/
         });
     }
 }
