a349a1bfb0bd74a907941009e4173c293b2cbeea
Nikasvirid <sviridka-vp@mail.ru>
2022-06-30 22:32:52 +0300

    fix:add interface

--- src/index.js
+++ src/index.js
@@ -3,3 +3,22 @@ import './global.css';
 
 const app = new App();
 app.start();
+
+
+
+ 
+
+
+  const options = {
+    method: 'GET',
+    mode: 'cors'
+};
+fetch(`http://localhost:8080/`, options)
+.then((response) => {
+console.log('response', response);
+return response.json();
+})
+.then((data) => {
+console.log('data', data);
+})
+.catch(() => { console.log('error') }); 
