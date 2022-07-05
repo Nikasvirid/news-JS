499c84865dae23c7b8c9ce1ca317abc6136445d7
Nikasvirid <sviridka-vp@mail.ru>
2022-07-06 01:38:29 +0300

    fix eslint

--- webpack.config.js
+++ webpack.config.js
@@ -38,4 +38,5 @@ module.exports = ({ mode }) => {
     const envConfig = isProductionMode ? require('./webpack.prod.config') : require('./webpack.dev.config');
 
     return merge(baseConfig, envConfig);
+    
 };
